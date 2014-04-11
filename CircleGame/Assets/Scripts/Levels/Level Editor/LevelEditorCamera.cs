2f308fa99e4ec5a301756d6012a3763af8790aad
Ryan Harrington <krooked590@gmail.com>
2014-04-11 15:50:02 -0700

    this addresses issue #7
    i still have not found the correct solution for this problem but i have
    ghetto rigged it so at least when fully zoomed out you can at least see
    the ground/walls

--- CircleGame/Assets/Scripts/Levels/Level Editor/LevelEditorCamera.cs
+++ CircleGame/Assets/Scripts/Levels/Level Editor/LevelEditorCamera.cs
@@ -8,13 +8,13 @@ public class LevelEditorCamera : MonoBehaviour {
     public tk2dCamera mainCam;
     public tk2dCamera bgCam;
     public Vector4 cameraBounds;
-    private Vector4 currentCameraBounds;
+    private Vector4? currentCameraBounds;
     public float accelerant = 1.0f;
     public float zoomSpeed = 1.0f;
     public float maxZoom = .5f;
     public bool shouldTrack { get; set; }
 
-    private void Awake() { 
+    private void Awake() {
         root = transform;
         currentCameraBounds = cameraBounds;
     }
@@ -32,6 +32,7 @@ public class LevelEditorCamera : MonoBehaviour {
     }
 
     public void Zoom(float scroll) {
+        if(!currentCameraBounds.HasValue) currentCameraBounds = cameraBounds;
         if(scroll != 0) {
             float startZoom = mainCam.ZoomFactor;
             if(mainCam.ZoomFactor + scroll < maxZoom) {
@@ -45,19 +46,31 @@ public class LevelEditorCamera : MonoBehaviour {
                 bgCam.ZoomFactor += scroll;
             }
 
-            if(mainCam.ZoomFactor <= startZoom) currentCameraBounds = cameraBounds * mainCam.ZoomFactor;
-            else if(mainCam.ZoomFactor >= startZoom) currentCameraBounds = cameraBounds / mainCam.ZoomFactor;
+            if(mainCam.ZoomFactor != startZoom) {
+                Vector4 newBounds = Vector4.zero;
+                //newBounds.x = cameraBounds.x * mainCam.ZoomFactor;
+                //newBounds.y = cameraBounds.y * mainCam.ZoomFactor;
+                //newBounds.z = cameraBounds.z * mainCam.ZoomFactor;
+                //newBounds.w = cameraBounds.w * mainCam.ZoomFactor;
 
+                newBounds.x = cameraBounds.x * mainCam.ZoomFactor;
+                newBounds.y = cameraBounds.y * mainCam.ZoomFactor;
+                newBounds.z = cameraBounds.z * mainCam.ZoomFactor;
+                newBounds.w = cameraBounds.w * mainCam.ZoomFactor;
+
+                currentCameraBounds = newBounds;
+            }
+            Debug.Log(currentCameraBounds.Value);
             RepositionCamera();
         }
     }
 
     public void RepositionCamera() {
-        if(cameraPos.x < currentCameraBounds.x) cameraPos.x = currentCameraBounds.x;
-        else if(cameraPos.x > currentCameraBounds.z) cameraPos.x = currentCameraBounds.z;
+        if(cameraPos.x < currentCameraBounds.Value.x) cameraPos.x = currentCameraBounds.Value.x;
+        else if(cameraPos.x > currentCameraBounds.Value.z) cameraPos.x = currentCameraBounds.Value.z;
 
-        if(cameraPos.y > currentCameraBounds.y) cameraPos.y = currentCameraBounds.y;
-        else if(cameraPos.y < currentCameraBounds.w) cameraPos.y = currentCameraBounds.w;
+        if(cameraPos.y > currentCameraBounds.Value.y) cameraPos.y = currentCameraBounds.Value.y;
+        else if(cameraPos.y < currentCameraBounds.Value.w) cameraPos.y = currentCameraBounds.Value.w;
 
         cameraPos.z = -10;
         root.position = cameraPos;
